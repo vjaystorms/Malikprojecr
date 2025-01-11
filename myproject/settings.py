7c31e44b9be6a0670122c61087bb4d64b44c40ac
victor <victorsalimon6@gmail.com>
2025-01-11 23:10:00 +0100

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,7 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'myapp',  # Your app
 ]
 
 MIDDLEWARE = [
